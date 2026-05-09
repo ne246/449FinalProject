05a3e255d9291823c10bde58584c6b28c0c1e4a4
Ricardo Pena <pena_ricky@csu.fullerton.edu>
2026-05-08 20:42:24 -0700

    Add authentication request and response models

---
+++ src/main/java/com/cpsc449/backendapi/auth/AuthResponse.java
@@ -0,0 +1,4 @@
+package com.cpsc449.backendapi.auth;
+
+public record AuthResponse(String token) {
+}
---
+++ src/main/java/com/cpsc449/backendapi/auth/AuthenticatedUser.java
@@ -0,0 +1,4 @@
+package com.cpsc449.backendapi.auth;
+
+public record AuthenticatedUser(Long id, String email) {
+}
